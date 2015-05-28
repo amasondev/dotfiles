d4609c25be12a1f1f667b842a2fae266dce46f1c
veridiam <alex@accelgames.com>
2015-05-27 17:31:38 -0700

    new source theme, xscreensaver conf, compton fixes

--- .config/awesome/applications.lua
+++ .config/awesome/applications.lua
@@ -4,6 +4,7 @@ myappmenu = {
 	{ "Mumble" , "mumble" },
 	{ "PlayOnLinux", "playonlinux" },
 	{ "Skype" , "skype" },
+	{ "Steam" , "steam" },
 	{ "Transmission" , "transmission-gtk" },
 	{ "VirtualBox" , "virtualbox" },
 	{ "World of Warcraft", "playonlinux --run WoW" },
@@ -14,6 +15,7 @@ settingsmenu = {
         { "Awesome" , "xdg-open .config/awesome/rc.lua" },
 	{ "Disks" , "gksu gparted" },
         { "Edit Applications" , "xdg-open .config/awesome/applications.lua" },
+	{ "Login Screen", "gksu lightdm-gtk-greeter-settings" },
         { "Network" , "nm-connection-editor" },
 	{ "Sound" , "pavucontrol" },
 	{ "Task Manager", "xfce4-taskmanager" },
@@ -29,8 +31,9 @@ editor_cmd = terminal .. " -e " .. editor
 autorun = {
 	"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
 	"rm ~/.config/pulse/*stream-volumes.tdb",
+	"xscreensaver -nosplash",
 	"compton -b",
 	"nm-applet",
 	"pasystray",
-	"skype",        
+	"skype",
 }
